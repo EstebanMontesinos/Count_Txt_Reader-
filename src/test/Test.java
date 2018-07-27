b7856ad8264057bc7ff7218c65eead4784ba002d
Esteban Montesinos <32073048+EstebanMontesinos@users.noreply.github.com>
2018-07-27 16:16:10 -0500

    first reader

---
+++ src/test/Test.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+import java.io.*;
+/**
+ *
+ * @author estebanmontesinos
+ */
+public class Test {
+public static void main(String [] args) throws FileNotFoundException, IOException{
+     {
+        File file = new File("/Users/estebanmontesinos/Desktop/test.txt");
+        FileInputStream fileStream = new FileInputStream(file);
+        InputStreamReader input = new InputStreamReader(fileStream);
+        BufferedReader reader = new BufferedReader(input);
+         
+        String line;
+         
+        // Initializing counters
+        int countWord = 0;
+        int sentenceCount = 0;
+        int characterCount = 0;
+        int paragraphCount = 1;
+        int whitespaceCount = 0;
+         
+        // Reading line by line from the 
+        // file until a null is returned
+        while((line = reader.readLine()) != null)
+        {
+            if(line.equals(""))
+            {
+                paragraphCount++;
+            }
+            if(!(line.equals("")))
+            {
+                 
+                characterCount += line.length();
+                 
+                // \\s+ is the space delimiter in java
+                String[] wordList = line.split("\\s+");
+                 
+                countWord += wordList.length;
+                whitespaceCount += countWord -1;
+                 
+                // [!?.:]+ is the sentence delimiter in java
+                String[] sentenceList = line.split("[!?.:]+");
+                 
+                sentenceCount += sentenceList.length;
+            }
+        }
+         
+        System.out.println("Total word count = " + countWord);
+        System.out.println("Total number of sentences = " + sentenceCount);
+        System.out.println("Total number of characters = " + characterCount);
+        System.out.println("Number of paragraphs = " + paragraphCount);
+        System.out.println("Total number of whitespaces = " + whitespaceCount);
+    }
+}
+
+}
+      
